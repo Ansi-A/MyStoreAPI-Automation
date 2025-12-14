e2552325b02dfb911dcba2500681bb7f3c06c54a
Ansi-A <mmssaallmmaann12@gmail.com>
2025-12-14 17:37:48 +0500

    Add GitHub Actions CI for API test automation

--- src/utils/conftest.py
+++ src/utils/conftest.py
@@ -1,7 +1,7 @@
 
 
 class Configuration:
-    URL = "http://127.0.0.1:8000/"
+    URL = "http://127.0.0.1:9000/"
     ADMIN_BODY={"username":"admin_user","password":"password123"}
     USER_BODY={"username":"Jessica_Jimenez","password":"password123"}
 
