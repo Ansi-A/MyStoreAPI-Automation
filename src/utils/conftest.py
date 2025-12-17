c21526bdff9553cff45a154469801be58b1a1f75
Ansi-A <mmssaallmmaann12@gmail.com>
2025-12-18 00:05:14 +0500

    Add GitHub Actions CI for API test automation

--- src/utils/conftest.py
+++ src/utils/conftest.py
@@ -1,9 +1,14 @@
-
+import os
 
 class Configuration:
-    URL = "http://127.0.0.1:9000/"
-    ADMIN_BODY={"username":"admin_user","password":"password123"}
-    USER_BODY={"username":"Jessica_Jimenez","password":"password123"}
+    URL = os.getenv("BASE_URL", "http://127.0.0.1:9000/")
 
+    ADMIN_BODY = {
+        "username": os.getenv("ADMIN_USERNAME", "admin_user"),
+        "password": os.getenv("ADMIN_PASSWORD", "password123")
+    }
 
-
+    USER_BODY = {
+        "username": os.getenv("USER_USERNAME", "Jessica_Jimenez"),
+        "password": os.getenv("USER_PASSWORD", "password123")
+    }
